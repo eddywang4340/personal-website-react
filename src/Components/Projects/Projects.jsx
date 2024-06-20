ced49744768f83a331fa10331096dd1ced166be2
edwardwang4340 <eddywang530@gmail.com>
2024-06-20 15:25:54 -0400

    added unique popup functionality and added first project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,14 +6,62 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Projects = () => {
 
-    const [isOpen, setOpen] = useState(false);
+    const [isOpen1, setOpen1] = useState(false);
+    const [isOpen2, setOpen2] = useState(false);
+    const [isOpen3, setOpen3] = useState(false);
+    const [isOpen4, setOpen4] = useState(false);
+    const [isOpen5, setOpen5] = useState(false);
+    const [isOpen6, setOpen6] = useState(false);
 
     return (
         <>
-        {isOpen ? (
+        {isOpen1 ? (
             <div className="popup-project">
-                <span onClick={()=> setOpen(false)}>&times;</span>
-                <div className="popup"></div>
+                <span onClick={()=> setOpen1(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[0].w_name}</h>
+                    <p>{projects_data[0].w_description}</p>
+                </div>
+            </div>
+        ) : isOpen2 ? (
+            <div className="popup-project">
+                <span onClick={()=> setOpen2(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[1].w_name}</h>
+                    <p>{projects_data[1].w_description}</p>
+                </div>
+            </div>
+        ) : isOpen3 ? (
+            <div className="popup-project">
+                <span onClick={()=> setOpen3(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[2].w_name}</h>
+                    <p>{projects_data[2].w_description}</p>
+                </div>
+            </div>
+        ) : isOpen4 ? (
+            <div className="popup-project">
+                <span onClick={()=> setOpen4(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[3].w_name}</h>
+                    <p>{projects_data[3].w_description}</p>
+                </div>
+            </div>
+        ) : isOpen5 ? (
+            <div className="popup-project">
+                <span onClick={()=> setOpen5(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[4].w_name}</h>
+                    <p>{projects_data[4].w_description}</p>
+                </div>
+            </div>
+        ) : isOpen6 ? (
+            <div className="popup-project">
+                <span onClick={()=> setOpen6(false)}>&times;</span>
+                <div className="popup">
+                    <h>{projects_data[5].w_name}</h>
+                    <p>{projects_data[5].w_description}</p>
+                </div>
             </div>
         ) : (
             <div id='projects' className='projects'>
@@ -23,7 +71,14 @@ const Projects = () => {
                 </div>
                 <div className="projects-container">
                     {projects_data.map((work, index)=>{
-                        return <img key={index} src={work.w_img} alt="" onClick={()=> setOpen(true)}/>
+                        return <img key={index} src={work.w_img} alt="" onClick={()=> {
+                            if (index == 0) setOpen1(true);
+                            if (index == 1) setOpen2(true);
+                            if (index == 2) setOpen3(true);
+                            if (index == 3) setOpen4(true);
+                            if (index == 4) setOpen5(true);
+                            if (index == 5) setOpen6(true);
+                        }}/>
                     })}
                 </div>
                 <div className="projects-showmore">
--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -8,32 +8,38 @@ import project6_img from '../assets/project_6.svg'
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
+        w_name:"Bird Classifier - Machine Learning",
+        w_description: "Learned basics of machine learning through Google's crash course (introduction to TensorFlow and training/testing data).\n Categorized bird species into NumPy arrays and partitioned the data set into a training data and a testing data.\n Used TensorFlow Keras to build a sequential model and added Conv2D filters to increase the depth of image perception.\n Saved evaluated and trained model as a JSON file to be used to predict the bird species of a singular image",
         w_img:project1_img
     },
     {
         w_no:2,
-        w_name:"Web design",
+        w_name:"Web design 2",
+        w_description: "Web design 2's description",
         w_img:project2_img
     },
     {
         w_no:3,
-        w_name:"Web design",
+        w_name:"Web design 3",
+        w_description: "Web design 3's description",
         w_img:project3_img
     },
     {
         w_no:4,
-        w_name:"Web design",
+        w_name:"Web design 4",
+        w_description: "Web design 4's description",
         w_img:project4_img
     },
     {
         w_no:5,
-        w_name:"Web design",
+        w_name:"Web design 5",
+        w_description: "Web design 5's description",
         w_img:project5_img
     },
     {
         w_no:6,
-        w_name:"Web design",
+        w_name:"Web design 6",
+        w_description: "Web design 6's description",
         w_img:project6_img
     },
 ]
